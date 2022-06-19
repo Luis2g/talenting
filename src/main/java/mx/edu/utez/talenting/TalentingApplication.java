f39475bb0f6b9885ac7e5240c8fa24dbca3b7029
Luis2g <luisjimenez2g@gmail.com>
2022-06-18 20:21:15 -0500

    First local commit

---
+++ src/main/java/mx/edu/utez/talenting/TalentingApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.talenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentingApplication.class, args);
+	}
+
+}
